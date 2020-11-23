f1c19d43d65ef239ecf5a8ac1cf96791d2386bf1
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-11-23 06:59:14 +0530

    added express and webhook

--- express-server.js
+++ express-server.js
@@ -1,41 +1,25 @@
+const express = require('express');
 const {
     Telegraf,
     Stage,
-    session,
-    // Extra,
-    // Markup 
+    session
 } = require('telegraf');
 
-// const WizardScene = require('telegraf/scenes/wizard');
-// const axios = require('axios').default;
-const {
-    generateUpdateMiddleware
-} = require('telegraf-middleware-console-time');
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV !== "production") {
-    const dotenv = require('dotenv').config();
-};
-
-
 const bot = new Telegraf(process.env.bot_token);
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV !== "production") {
-    bot.use(generateUpdateMiddleware());
-};
 const pool = require('./db');
 const createAddDocument = require("./scenes/addDocument");
 const createAbruptCloseScene = require("./scenes/abruptClose");
 const createCloseScene = require("./scenes/close");
 
+
 const stage = new Stage([
-    createAddDocument("ADD_DOCUMENT", async(ctx) => {
+    createAddDocument("ADD_DOCUMENT", async (ctx) => {
         ctx.reply("Completed the task of adding document[s] exiting now");
-        return("CLOSE");
+        return ("CLOSE");
     }),
     createAbruptCloseScene("ABRUPT_CLOSE", () => "CLOSE"),
     createCloseScene("CLOSE", () => {}),
 ]);
-
 
 bot.use(session());
 bot.use(stage.middleware());
@@ -44,13 +28,9 @@ bot.catch((err, ctx) => {
     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err)
 });
 
-
-
 bot.start((ctx) => {
     ctx.reply('Welcome');
-    // console.log(ctx.startPayload);
 });
-bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.on('document', (ctx, next) => {
@@ -76,11 +56,20 @@ bot.on('document', (ctx, next) => {
 });
 
 bot.command('/close', (ctx, next) => {
-    // ctx.telegram.leaveChat(ctx.chat.id);
     ctx.reply("No work was going on. ZZzzz");
 
 });
 
 bot.command('newdoc', Stage.enter("ADD_DOCUMENT"));
-bot.launch();
-bot.startPolling();
+
+bot.telegram.setWebhook('https://caseFileBot.ml/bot');
+
+const app = express();
+
+app.get('/', (req, res) => res.redirect('https://github.com/Omkaragrawal/CaseFiles-telegraf'));
+
+app.use(bot.webhookCallback('/bot'));
+
+app.listen(process.env.PORT, () => {
+    console.log('Example app listening on port 3000!')
+});
